0284ddf669fd6057e75d2db2a60a42edc3026277
NilakashSarkar <nilakashsarkar2@gmail.com>
2018-10-29 14:03:43 +0530

    all is ok

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./route/authRoute')(app)
+//By default
+app.get('/',(req,res) =>{
+    res.send({'welcome ':'Nilakash'})
+})
 const PORT=process.env.PORT || 5000
 console.log('Database Connection Established Port No-'+PORT)
 app.listen(PORT);
