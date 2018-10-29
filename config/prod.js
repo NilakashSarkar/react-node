04f0b3f5f319dd74506314c5e41afd2d04f4452e
NilakashSarkar <nilakashsarkar2@gmail.com>
2018-10-29 13:31:40 +0530

    m

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports={
+    googleClientId:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecrete:process.env.GOOGLE_CLIENT_SECRETE,
+    mongoURL:process.env.mongoURL,
+    cookieKey:process.env.COOKIE_KEY,
+   
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const keys=require('./config/keys')
+const cookieSession=require('cookie-session')
+const passport=require('passport')
+const app=express()
+require('./models/User')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURL)
+
+app.use(cookieSession({
+    maxAge:60 * 24 * 60 * 60 * 1000,
+    keys:[keys.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./route/authRoute')(app)
+const PORT=process.env.PORT || 5000
+console.log('Database Connection Established Port No-'+PORT)
+app.listen(PORT);
