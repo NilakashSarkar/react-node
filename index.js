73814aa03a6eba3a1d159efe5bfd405a74a3998e
NilakashSarkar <nilakashsarkar2@gmail.com>
2018-11-08 19:16:29 +0530

    fullstack

--- index.js
+++ index.js
@@ -22,23 +22,23 @@ app.use(passport.session())
 
 require('./route/authRoute')(app)
 require('./route/billingRoute')(app)
-// require('./route/registation')(app)
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'));
-//     // const path=require('path');
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     });
-// }
+require('./route/registation')(app)
 if(process.env.NODE_ENV === 'production'){
-    const path = require('path'); //We need path earlier for this!
-    app.use(express.static(path.join(__dirname, '/client/build')));
-    //No more changes from here on now
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client'
-, 'build', 'index.html'))
+    app.use(express.static('client/build'));
+    // const path=require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     });
 }
+// if(process.env.NODE_ENV === 'production'){
+//     const path = require('path'); //We need path earlier for this!
+//     app.use(express.static(path.join(__dirname, '/client/build')));
+//     //No more changes from here on now
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client'
+// , 'build', 'index.html'))
+//     });
+// }
 
 const PORT=process.env.PORT || 5000
 console.log('Database Connection Established Port No-'+PORT)
