bd44f0893b3fe2bed882ab986c7665538ec6e015
NilakashSarkar <nilakashsarkar2@gmail.com>
2018-11-08 19:09:26 +0530

    try to solve

--- index.js
+++ index.js
@@ -23,11 +23,20 @@ app.use(passport.session())
 require('./route/authRoute')(app)
 require('./route/billingRoute')(app)
 // require('./route/registation')(app)
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
+//     // const path=require('path');
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     });
+// }
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    // const path=require('path');
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    const path = require('path'); //We need path earlier for this!
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    //No more changes from here on now
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client'
+, 'build', 'index.html'))
     });
 }
 
